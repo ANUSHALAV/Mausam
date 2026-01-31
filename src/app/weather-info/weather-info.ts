09841f901b8308fb09afdd024838eb1440a930ae
ANUSHALAV <bhattanushalav@gmail.com>
2026-01-31 16:37:51 +0530

    integrate weather api

--- src/app/weather-info/weather-info.ts
+++ src/app/weather-info/weather-info.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { WeatherService } from '../weather-service';
 
 @Component({
   selector: 'app-weather-info',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './weather-info.html',
   styleUrl: './weather-info.css',
+  standalone: true
 })
-export class WeatherInfo {
+export class WeatherInfo implements OnInit {
 
+  weatherInfo: any = {};
+  cityName: string = 'Delhi';
+
+  constructor(private weatherService: WeatherService) { }
+
+  ngOnInit(): void {
+    this.getWeather(this.cityName);
+  }
+
+  getWeather(cityName: string) {
+    cityName = cityName.trim();
+    if (cityName != null && cityName != '' && cityName != undefined) {
+      this.weatherService.GetWeatherData(cityName).subscribe((data: any) => {
+        this.weatherInfo = {
+          City: data.name,
+          Country: data.sys.country,
+          Temp: data.main.temp + '°C',
+          Description: data.weather[0].description,
+          Humidity: data.main.humidity + '%',
+          Wind: data.wind.speed + ' m/s',
+          Pressure: data.main.pressure + ' hPa'
+        };
+      });
+    } else {
+      alert('Please enter a valid city name');
+    }
+  }
 }
--- src/app/weather-service.ts
+++ src/app/weather-service.ts
@@ -1,8 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
-  
+
+  constructor(private httpClient: HttpClient) { }
+
+  GetWeatherData(cityName: string): any {
+      let headers = {
+        'content-type': 'application/json',
+        'x-rapidapi-key': '4f3e61d02amsh4a3d50d49991f94p104451jsn76ca7cd7f596',
+        'x-rapidapi-host': 'yahoo-weather5.p.rapidapi.com'
+      }
+      return this.httpClient.get('https://yahoo-weather5.p.rapidapi.com/weather?location=' + cityName + '&format=json&u=f', { headers });
+  }
 }

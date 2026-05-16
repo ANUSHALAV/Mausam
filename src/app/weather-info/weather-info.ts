edac2dbf18e601e8d04a33ec5de513a37705a02c
ANUSHALAV <bhattanushalav@gmail.com>
2026-05-16 17:10:50 +0530

    change API method calling

--- src/app/weather-info/weather-info.ts
+++ src/app/weather-info/weather-info.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-weather-info',
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './weather-info.html',
   styleUrl: './weather-info.css',
   standalone: true
@@ -26,24 +26,29 @@ export class WeatherInfo implements OnInit {
   getWeather(cityName: string) {
     cityName = cityName.trim();
     if (cityName != null && cityName != '' && cityName != undefined) {
-      this.weatherService.GetWeatherData(cityName).subscribe((data: any) => {
-        if (data == null || data == undefined || data == '') {
-          alert('Please enter a valid city name');
-          return;
-        } else {
-          this.weatherInfo = data;
-          if (data?.current_observation?.condition?.text == 'Mostly Sunny'||data?.current_observation?.condition?.text == 'Sunny') {
-            this.weatherInfo.weatherImage = '../../assets/weather/Sunny.png';
+      this.weatherService.GetWeatherData(cityName).subscribe({
+        next: (data: any) => {
+          if (data == null || data == undefined || data == '') {
+            alert('Please enter a valid city name');
+            return;
+          } else {
+            this.weatherInfo = data;
+            if (data?.current_observation?.condition?.text == 'Mostly Sunny' || data?.current_observation?.condition?.text == 'Sunny') {
+              this.weatherInfo.weatherImage = '../../assets/weather/Sunny.png';
+            }
+            else if (data?.current_observation?.condition?.text == 'Cloudy' || data?.current_observation?.condition?.text == 'Partly Cloudy') {
+              this.weatherInfo.weatherImage = '../../assets/weather/Cloudy.png';
+            }
+            else if (data?.current_observation?.condition?.text == 'Rainy') {
+              this.weatherInfo.weatherImage = '../../assets/weather/Rainy.png';
+            }
+            else if (data?.current_observation?.condition?.text == 'Snow') {
+              this.weatherInfo.weatherImage = '../../assets/weather/Snow.png';
+            }
           }
-          else if (data?.current_observation?.condition?.text == 'Cloudy') {
-            this.weatherInfo.weatherImage = '../../assets/weather/Cloudy.png';
-          }
-          else if (data?.current_observation?.condition?.text == 'Rainy') {
-            this.weatherInfo.weatherImage = '../../assets/weather/Rainy.png';
-          }
-          else if (data?.current_observation?.condition?.text == 'Snow') {
-            this.weatherInfo.weatherImage = '../../assets/weather/Snow.png';
-          }
+        },
+        error: (err:any) => {
+          console.log(err);
         }
       });
     } else {
